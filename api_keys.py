bd5cfe0d6617cdf90a71166ba96d4403731497ce
ayiemba ambaza <ambazas@gmail.com>
2020-12-20 23:11:53 -0500

    homework update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f3603bd91a3e08d075552f0b0acabf09!"
+weather_api_key = "f3603bd91a3e08d075552f0b0acabf09"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAyRhJgYujD273w646BEeicj2TxsE5qesg"
